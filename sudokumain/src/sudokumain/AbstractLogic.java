3b190db0152e124f49e5169199ca3b33c3bae56f
Edwin Gabriel Portillo <weaselSSH@gmail.com>
2025-10-31 09:13:33 -0600

    abstractlogic

--- sudokumain/src/sudokumain/AbstractLogic.java
+++ sudokumain/src/sudokumain/AbstractLogic.java
@@ -2,17 +2,49 @@ package sudokumain;
 
 public abstract class AbstractLogic {
 
+    //constantes
+    protected final int TAM = 9;
+    protected final int TAM_CAJA = 3;
+
+    //variable
     protected int[][] tablero;
-    protected boolean filasOK;
-    protected boolean columnasOK;
-    protected boolean cajasOK;
-    protected boolean ganado;
 
-    public abstract void juegoGanado();
+    protected AbstractLogic(int[][] tablero) {
+        this.tablero = tablero;
+    }
 
-    public abstract void columnaValida();
+    public final boolean validar() {
+        boolean okFilas = validarFilas();
+        boolean okColumnas = validarColumnas();
+        boolean okCajas = validarCajas();
+        boolean sinCeros = sinCeros();
+        return okFilas && okColumnas && okCajas && sinCeros;
+    }
 
-    public abstract void cajaValida();
+    protected boolean sinCeros() {
+        for (int fila = 0; fila < TAM; fila++) {
+            for (int col = 0; col < TAM; col++) {
+                if (tablero[fila][col] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    public abstract void filaValida();
+    //estas funciones se heredaran en logicasudoku
+    protected abstract boolean validarFilas();
+
+    protected abstract boolean validarColumnas();
+
+    protected abstract boolean validarCajas();
+
+    //por si acaso
+    public int[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(int[][] nuevoTablero) {
+        this.tablero = nuevoTablero;
+    }
 }
