2a07bc525c15240a5701c5fd67a44415c0ff3d9c
Andrea Martinez <andreamartinezisj28@gmail.com>
2025-10-31 11:00:27 -0600

    Menu inicial cambios

--- sudokumain/src/sudokumain/MenuInicial.java
+++ sudokumain/src/sudokumain/MenuInicial.java
@@ -4,35 +4,45 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-
-
-
 
 /**
  *
  * @author gaat1
  */
-public class MenuInicial extends BaseGUI{
-    
-    public MenuInicial(){
-        
-        super("Menu de Inicio", 400, 600);
+public class MenuInicial extends BaseGUI {
 
-        JPanel panel = new JPanel();
+    public MenuInicial() {
+
+        super("Menu de Inicio", 700, 600);
+      
+ JPanel panel = createPanelPrincipal();
+        panel.setLayout(new BorderLayout(10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
+        JLabel titulo = createLabel("MENU INICIAL", 26, Font.BOLD, new Color(33, 33, 33));
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(titulo, BorderLayout.NORTH);
+        
+       
+        
+        
         JButton Facil = createBtn("Facil");
-    
+
         JButton Medio = createBtn("Medio");
-        
-        
+
         JButton Dificil = createBtn("Dificil");
         JButton Salir = createBtn("Salir");
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        
+
         Facil.setAlignmentX(Component.CENTER_ALIGNMENT);
         Medio.setAlignmentX(Component.CENTER_ALIGNMENT);
         Dificil.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -47,27 +57,26 @@ public class MenuInicial extends BaseGUI{
         panel.add(Box.createVerticalStrut(10));
         panel.add(Salir);
         panel.add(Box.createVerticalGlue());
-        
-        
-        Facil.addActionListener(e ->{ 
-            SwingUtilities.invokeLater(()->{
-              
-            }); 
+
+        Facil.addActionListener(e -> {
+            SwingUtilities.invokeLater(() -> {
+
+            });
         });
-        Medio.addActionListener(e ->{ 
-            SwingUtilities.invokeLater(()->{
-              
-            }); 
+        Medio.addActionListener(e -> {
+            SwingUtilities.invokeLater(() -> {
+
+            });
         });
-        Dificil.addActionListener(e ->{ 
-            SwingUtilities.invokeLater(()->{
-              
-            }); 
+        Dificil.addActionListener(e -> {
+            SwingUtilities.invokeLater(() -> {
+
+            });
         });
-        Salir.addActionListener(e ->{ 
-            SwingUtilities.invokeLater(()->{
-              
-            }); 
+        Salir.addActionListener(e -> {
+            SwingUtilities.invokeLater(() -> {
+
+            });
         });
     }
 }
