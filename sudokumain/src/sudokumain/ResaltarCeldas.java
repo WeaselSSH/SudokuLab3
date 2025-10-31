dfac448c063ad25ca5ec25484a72c2da69254a73
carmengabrie7 <carmenelisa.gabrie7@gmail.com>
2025-10-31 09:28:29 -0600

    Avance en clase para resaltar celdas

--- sudokumain/src/sudokumain/ResaltarCeldas.java
+++ sudokumain/src/sudokumain/ResaltarCeldas.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sudokumain;
 
-/**
- *
- * @author DELL
- */
+import javax.swing.JButton;
+import java.awt.Color;
+
 public class ResaltarCeldas {
+  //proposito: Cuando se presione una celda, se resalte toda la columna y fila en la que se encuentra, tipo cruz.
+    JButton[][] celdas; //los botones del tablero
+    private Color colorFila = new Color(247,181,189);
+    private Color colorColumna = new Color(247,219,181);
+    private Color colorBloque = new Color(247,181,212); //esto va a resaltar el bloque donde se encuentra la celda.
+    private Color colorDefault = Color.white;
     
+   //constructor
+    public ResaltarCeldas (JButton[][]celdas){
+        this.celdas=celdas;
+    }
+    
+    //metodo - se encarga de por default colorear las celdas de blanco.
+    public void limpiarCeldas(){
+        //para que recorra todas las filas
+        for (int i=0; i<9; i++){
+            //para que recorra todas las columnas
+            for (int j=0;i<9; j++){
+                celdas[i][j].setBackground(colorDefault);
+                celdas[i][j].setOpaque(true); //se encarga de que se pinte complementamente y no queda transparente.
+            }
+        }
+    }
+    
+    //metodo principal
+    public void colorearCeldas(){
+        limpiarCeldas();
+        
+    }
 }
