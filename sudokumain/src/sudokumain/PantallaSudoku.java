04a9e90da769fee4e5ef94cc3b27b25d20408b5d
Andrea Martinez <andreamartinezisj28@gmail.com>
2025-10-31 08:55:02 -0600

    Avaces en el GUI

--- sudokumain/src/sudokumain/PantallaSudoku.java
+++ sudokumain/src/sudokumain/PantallaSudoku.java
@@ -5,11 +5,23 @@
 package sudokumain;
 
 import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
  * @author andre
  */
-public class PantallaSudoku extends JFrame {
-    
+public class PantallaSudoku extends BaseGUI {
+
+    public PantallaSudoku() {
+        super("Sudoku", 600, 500);
+        JPanel panel = createPanelPrincipal();
+        panel.setLayout(new BorderLayout(10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        JLabel titulo = createLabel("Sudoku", 22, Font.BOLD, new Color(33, 33, 33));
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(titulo, BorderLayout.NORTH);
+    }
+
 }
---
+++ sudokumain/src/sudokumain/SudokuMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sudokumain;
+
+/**
+ *
+ * @author andre
+ */
+public class SudokuMain {
+
+    public static void main(String[] args) {
+        new PantallaSudoku().setVisible(true);
+    }
+}
